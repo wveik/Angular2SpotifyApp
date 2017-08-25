cfed776053d06554f29ab4b72f55ba4447d1ab74
wveik <kataev_06@list.ru>
2017-08-25 14:33:50 +0300

    add search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -18,7 +19,8 @@ import { routing } from './app.routing';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
     templateUrl: './search.component.html'
 })
 export class SearchComponent {
-  
+    searchStr:any;
+
+    searchMusic(){
+     console.log(this.searchStr);   
+    }
 }
