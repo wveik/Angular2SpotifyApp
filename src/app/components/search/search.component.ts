aa2e8836b66162fb096fdbcbd79f32efe0ae1d55
wveik <kataev_06@list.ru>
2017-08-25 19:40:19 +0300

    complete

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AudioscrobblerService } from '../../services/audioscrobbler.service';
+import {Router} from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -10,7 +11,7 @@ export class SearchComponent {
     albums: any[];
     searchStr:string;
 
-    constructor(private _audioscrobbler:AudioscrobblerService){
+    constructor(private _audioscrobbler:AudioscrobblerService, private _router: Router){
         
     }
 
@@ -23,6 +24,7 @@ export class SearchComponent {
         this._audioscrobbler.searchMusic(this.searchStr).subscribe(response => {           
             this.albums = response.topalbums.album;
             console.log(this.albums);
+            //this._audioscrobbler.artist = this.albums[0].artist;
         });
     }
 
@@ -31,5 +33,11 @@ export class SearchComponent {
         let result: string = album.image[3]["#text"];
         if(result == "") result ="http://www.wellesleysocietyofartists.org/wp-content/uploads/2015/11/image-not-found-300x225.jpg";
         return result;
+    }
+
+    myClick(album: any){
+        console.log(album);
+        this._audioscrobbler.artist = album.artist;
+        this._router.navigate(['about']);
     }
 }
